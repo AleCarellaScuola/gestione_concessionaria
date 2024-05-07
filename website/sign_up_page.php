f8747efddb385bb17ec9da267916b93882431f6a
ChrisRedBeard <134591143+ChrisRedBeard@users.noreply.github.com>
2024-05-07 11:06:13 +0200

    Add files via upload

--- website/sign_up_page.php
+++ website/sign_up_page.php
@@ -5,22 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://w3.p2hp.com/lib/w3.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+
 </head>
 
 <body>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-        <input type="text" name="rif_name" id="name" placeholder="Nome"><br>
-        <input type="text" name="rif_surname" id="surname" placeholder="Cognome"><br>
-        <input type="date" name="rif_birth_date" id="birth_date">: Data di nascita<br>
-        <input type="text" name="rif_address" id="address" placeholder = "Indirizzo"><br>
-        <select id = "province">
+    <div class="form-group">
+        <div class="form-floating mb-3">
+            
+             <label for="name">Nome</label>
+            <input type="text"  class="form-control" name="rif_name" id="name" placeholder="Nome"><br>
+        </div>
+        <div class="form-floating mb-3">
+            <label for="surname">Cognome</label>
+            <input type="text" class="form-control" name="rif_surname" id="surname" placeholder="Cognome"><br>
+        </div>
+         <div class="form-floating mb-3">
+                <label for="birth_date">Data di nascita</label><br>
+                <input type="text" class="form-control" name="rif_birth_date" id="birth_date" placeholder = "Data di nascita" onclick = "this.type = 'date'">
+            </div>
+         <div class="form-floating mb-3">
+         <label for="address">indirizzo</label><br>
+            <input type="text" class="form-control" name="rif_address" id="address" placeholder = "Insereisci il tuo indirizzo..."><br>
+         <select id = "province">
             <option w3-repeat = "province" name = "rif_province" value = "{{id_provincia}}">{{nome}}, {{acronimo}}</option>
         </select>
+        </div>
         <br>
         <input type="email" name="rif_email" id="email" placeholder="email"><br>
         <input type="password" name="rif_psw" id="psw" placeholder="password"><br>
+        </div>
         <button type="button" id="registrati" name="send_data">Registrati</button>
     </form>
+</div>
 </body>
 
 <script>
