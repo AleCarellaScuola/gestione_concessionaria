1001c494d618b896908b02316746eeea2ece2b0d
alessandro carella <carellaalessandro1@gmail.com>
2024-05-07 18:40:15 +0200

    Cartella assets eliminata, eliminati file inutili, migliorata la visualizzazione della pagina e pagina menu.php continuata

--- website/sign_up_page.php
+++ website/sign_up_page.php
@@ -5,22 +5,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://w3.p2hp.com/lib/w3.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="sign_up_style.css">
+    
 </head>
 
 <body>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-        <input type="text" name="rif_name" id="name" placeholder="Nome"><br>
-        <input type="text" name="rif_surname" id="surname" placeholder="Cognome"><br>
-        <input type="date" name="rif_birth_date" id="birth_date">: Data di nascita<br>
-        <input type="text" name="rif_address" id="address" placeholder = "Indirizzo"><br>
-        <select id = "province">
-            <option w3-repeat = "province" name = "rif_province" value = "{{id_provincia}}">{{nome}}, {{acronimo}}</option>
-        </select>
-        <br>
-        <input type="email" name="rif_email" id="email" placeholder="email"><br>
-        <input type="password" name="rif_psw" id="psw" placeholder="password"><br>
-        <button type="button" id="registrati" name="send_data">Registrati</button>
-    </form>
+     <div class="riquadro">
+         <div class="form-group">
+            <h4>Sign-up</h4>
+            <div class="form-floating mb-3">        
+                <input type="text"  class="form-control" name="rif_name" id="name" placeholder = "Inserisci il tuo nome">
+                <label for="name">Nome</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="rif_surname" id="surname" plaecholder = "Inserisci il tuo cognome">
+                <label for="surname">Cognome</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="date" class="form-control" name="rif_birth_date" id="birth_date" >
+                <label for="birth_date">Data di nascita</label>
+                </div>
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="rif_address" id="address" plaecholder = "Inserisci il tuo indirizzo"><br>
+                <label for="address">Indirizzo</label>
+            </div>
+            <div class="form mb-3">
+                
+                <select id = "province" class="form-select">
+                <option selected>Seleziona la provincia</option>
+                <option w3-repeat = "province" name = "rif_province" value = "{{id_provincia}}">{{nome}}, {{acronimo}}</option>
+                </select>
+                
+            </div>
+            <br>
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" name="rif_email" id="email" placeholder="email"><br>
+                <label for="email">E-mail</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" name="rif_psw" id="psw" placeholder="password"><br>
+                <label for="psw">Password</label>
+            </div>
+            <button type="button" class = "btn btn-outline-primary" id="registrati" name="send_data">Registrati</button>
+        </div>
+    </div>
+ </form>
+
 </body>
 
 <script>
